35347666246adb05730c71257dcc2e9a5eaf2edb
Deadpool345 <87096532+Deadpool345@users.noreply.github.com>
2024-05-16 22:23:19 -0600

    merge

---
+++ view/components/navbar2.php
@@ -0,0 +1,35 @@
+<nav class=" bg-[#1B1A55]">
+    <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <div>
+            <img src="./images/logo.png" alt="Logo" class="" width="75">
+        </div>
+        <div class="hidden w-full md:block md:w-auto" id="navbar-default">
+            <ul
+                class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                <li>
+                    <a href="index.php"
+                        class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Inicio</a>
+                </li>
+                <li>
+                    <a href="horarios.php"
+                        class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Horarios</a>
+                </li>
+                <li>
+                    <a href="proxim.php"
+                        class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Prox.
+                        Estrenos</a>
+                </li>
+                <li>
+                    <a href="Snacks.php"
+                        class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Snacks</a>
+                </li>
+                <li>
+                    <a href="contacto.php"" class=" block py-2 px-3 text-gray-900 rounded hover:bg-gray-100
+                        md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white
+                        md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white
+                        md:dark:hover:bg-transparent">Contacto</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
